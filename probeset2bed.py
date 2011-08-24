9b19f5a9c4f9d7bc465e70626c8d493cac207872
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-08-24 12:51:32 +0100

    probeset2bed.py: explicitly write output to a file; changed fields that are written out to be chr/start/stop/name; use logging module for messaging.

--- probeset2bed.py
+++ probeset2bed.py
@@ -20,6 +20,7 @@ Create BED file from microarray probeset data.
 
 import os
 import sys
+import logging
 
 #######################################################################
 # Main program
@@ -33,10 +34,16 @@ if __name__ == "__main__":
             os.path.basename(sys.argv[0])
         sys.exit(1)
     probeset_file = sys.argv[1]
-    ##print "Index: %s" % probeset_file
+    logging.debug("Index: %s" % probeset_file)
 
-    # Process the probeset file
+    # Output file
+    bed_file = os.path.splitext(os.path.basename(probeset_file))[0]+".bed"
+    if os.path.exists(bed_file):
+        logging.warning("BED file '%s' will be overwritten" % bed_file)
+
+    # Process the probeset file and write to bed file
     fp = open(probeset_file,'r')
+    fo = open(bed_file,'w')
     for line in fp:
         if line.startswith(">probe_set:"):
             # Process probset line
@@ -45,16 +52,20 @@ if __name__ == "__main__":
             # First field should be "probeset"
             # e.g. >probe_set:HuEx-1_0-st-v2:2315101
             # Get the name and id
-            name = fields[0].split(':')[1]
-            id = fields[0].split(':')[2]
+            probeset_name = fields[0].split(':')[1]
+            probeset_id = fields[0].split(':')[2]
+            probeset = probeset_name+':'+probeset_id
             # Seqname
             seqname = fields[2].split('=')[1]
             # Start
             start = fields[3].split('=')[1]
             # Stop
             stop = fields[4].split('=')[1]
-            #
-            print "%s\t%s\t%s\t%s" % (id,start,stop,name)
+            # Write to output file
+            fo.write("%s\t%s\t%s\t%s\n" %
+                     (seqname,start,stop,probeset))
     fp.close()
+    fo.close()
 
     # Finished
+    sys.exit()
