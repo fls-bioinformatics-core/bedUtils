78f26d41ba390d43e8ebf268054f39047590e5f3
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-10-03 09:29:29 +0100

    Moved TabFile.py to common "share" directory and update bedGraphSplitter.py to add "share" to module search path.

--- bedGraphSplitter.py
+++ bedGraphSplitter.py
@@ -32,6 +32,12 @@ column.
 import os
 import sys
 import logging
+
+# Set up for local modules in "share"
+SHARE_DIR = os.path.abspath(
+    os.path.normpath(
+        os.path.join(os.path.dirname(sys.argv[0]),'..','share')))
+sys.path.append(SHARE_DIR)
 from TabFile import TabFile
 
 # Set default logging level and output
